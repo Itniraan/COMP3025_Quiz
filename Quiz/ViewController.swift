a017dfbe16b6f48254e28227525587caf2803478
student <student@students-mac.student.georgianc.on.ca>
2016-01-19 16:11:05 -0500

    Added ViewController Links

--- Quiz/ViewController.swift
+++ Quiz/ViewController.swift
@@ -9,13 +9,16 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet var questionLabel: UILabel!
+    @IBOutlet var answerLabel: UILabel!
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        print("The View Did Load!")
+    @IBAction func showNextQuestion(sender: AnyObject) {
+        
     }
-
-
+    
+    @IBAction func showAnswer (sender: AnyObject) {
+        
+    }
 
 
 }
